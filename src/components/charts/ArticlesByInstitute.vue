8cf091fdf4f0758ca5a99fd8b420e6b74c8315de
Otacílio Neto <otaneto@rogalabs.com>
2019-09-16 11:22:24 -0300

    created ArticlesByInstitute chart component

---
+++ src/components/charts/ArticlesByInstitute.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-layout column>
+    <h2>Total de artigos por IF</h2>
+    <ve-histogram
+      :data="chartData"
+      :settings="chartSettings"
+    />
+  </v-layout>
+</template>
+
+<script>
+import rows from '@/data/articles-by-institute.json';
+
+export default {
+  name: 'ArticlesByInstitute',
+  data() {
+    return {
+      chartData: {
+        columns: ['instituto', 'artigos'],
+        rows,
+      },
+    };
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,21 @@
 <template>
   <v-container>
-    <articles-by-area-and-institute />
+    <v-layout column>
+      <articles-by-area-and-institute />
+      <articles-by-institute />
+    </v-layout>
   </v-container>
 </template>
 
 <script>
 import ArticlesByAreaAndInstitute from '@/components/charts/ArticlesByAreaAndInstitute.vue';
+import ArticlesByInstitute from '@/components/charts/ArticlesByInstitute.vue';
 
 export default {
   name: 'Home',
   components: {
     ArticlesByAreaAndInstitute,
+    ArticlesByInstitute,
   },
 };
 </script>
