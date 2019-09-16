492f482ff36b4b015f0b10801d7b6403117ab0b6
Otacílio Neto <otaneto@rogalabs.com>
2019-09-16 10:37:33 -0300

    created first chart using mockaroo generated data

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
+import VCharts from 'v-charts';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
+
+Vue.use(VCharts);
 
 Vue.config.productionTip = false;
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,62 @@
 <template>
-  <div>Home</div>
+  <v-container>
+    <ve-histogram
+      :data="chartData"
+      :settings="chartSettings"
+    />
+  </v-container>
 </template>
 
 <script>
+import rows from './rows.json';
 
 export default {
+
   name: 'Home',
+  data() {
+    this.chartSettings = {
+      metrics: [
+        'Ciências Exatas e da Terra',
+        'Ciências Biológicas',
+        'Engenharias',
+        'Ciências da Saúde',
+        'Ciências Agrárias',
+        'Ciências Sociais Aplicadas',
+        'Ciências Humanas',
+        'Linguística, Letras e Artes',
+        'Outros',
+      ],
+      stack: {
+        areas: [
+          'Ciências Exatas e da Terra',
+          'Ciências Biológicas',
+          'Engenharias',
+          'Ciências da Saúde',
+          'Ciências Agrárias',
+          'Ciências Sociais Aplicadas',
+          'Ciências Humanas',
+          'Linguística, Letras e Artes',
+          'Outros',
+        ],
+      },
+    };
+    return {
+      chartData: {
+        columns: [
+          'institute',
+          'Ciências Exatas e da Terra',
+          'Ciências Biológicas',
+          'Engenharias',
+          'Ciências da Saúde',
+          'Ciências Agrárias',
+          'Ciências Sociais Aplicadas',
+          'Ciências Humanas',
+          'Linguística, Letras e Artes',
+          'Outros',
+        ],
+        rows,
+      },
+    };
+  },
 };
 </script>
