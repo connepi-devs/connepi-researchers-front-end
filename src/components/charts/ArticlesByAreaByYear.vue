d6881c068bddf4d8cc53ce47c9960cea11b68fc1
Otacílio Neto <otaneto@rogalabs.com>
2019-09-16 15:38:01 -0300

    added ArticlesByAreaByYear, needs fixes

---
+++ src/components/charts/ArticlesByAreaByYear.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-layout column align-center>
+    <h2>Total de artigos por área por ano</h2>
+    <ve-line :data="chartData" />
+  </v-layout>
+</template>
+
+<script>
+import rows from '@/data/articles-by-area-by-year.json';
+
+export default {
+  name: 'ArticlesByAreaByYear',
+  computed: {
+    chartData() {
+      return {
+        columns: [
+          'year',
+          'Ciências Exatas e da Terra',
+          'Ciências Biológicas',
+          'Engenharias',
+          'Ciências da Saúde',
+          'Ciências Agrárias',
+          'Ciências Sociais Aplicadas',
+          'Ciências Humanas',
+          'Linguística, Letras e Artes',
+          'Outros',
+        ],
+        rows,
+      };
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,18 +3,21 @@
     <v-layout column>
       <articles-by-area-and-institute />
       <articles-by-institute />
+      <articles-by-area-by-year />
     </v-layout>
   </v-container>
 </template>
 
 <script>
 import ArticlesByAreaAndInstitute from '@/components/charts/ArticlesByAreaAndInstitute.vue';
+import ArticlesByAreaByYear from '@/components/charts/ArticlesByAreaByYear.vue';
 import ArticlesByInstitute from '@/components/charts/ArticlesByInstitute.vue';
 
 export default {
   name: 'Home',
   components: {
     ArticlesByAreaAndInstitute,
+    ArticlesByAreaByYear,
     ArticlesByInstitute,
   },
 };
