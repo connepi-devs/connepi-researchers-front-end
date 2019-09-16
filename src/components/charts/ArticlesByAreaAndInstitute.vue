4d1b2df9b308f8ad54dd747723475cc4ae3e92e6
Otacílio Neto <otaneto@rogalabs.com>
2019-09-16 10:54:46 -0300

    created ArticlesByAreaAndInstitute component

---
+++ src/components/charts/ArticlesByAreaAndInstitute.vue
@@ -0,0 +1,59 @@
+<template>
+  <ve-histogram
+    :data="chartData"
+    :settings="chartSettings"
+  />
+</template>
+
+<script>
+import rows from '@/data/articles-by-area-and-institute.json';
+
+export default {
+  name: 'ArticlesByAreaAndInstitute',
+  data() {
+    this.chartSettings = {
+      metrics: [
+        'Ciências Exatas e da Terra',
+        'Ciências Biológicas',
+        'Engenharias',
+        'Ciências da Saúde',
+        'Ciências Agrárias',
+        'Ciências Sociais Aplicadas',
+        'Ciências Humanas',
+        'Linguística, Letras e Artes',
+        'Outros',
+      ],
+      stack: {
+        areas: [
+          'Ciências Exatas e da Terra',
+          'Ciências Biológicas',
+          'Engenharias',
+          'Ciências da Saúde',
+          'Ciências Agrárias',
+          'Ciências Sociais Aplicadas',
+          'Ciências Humanas',
+          'Linguística, Letras e Artes',
+          'Outros',
+        ],
+      },
+    };
+    return {
+      chartData: {
+        columns: [
+          'institute',
+          'Ciências Exatas e da Terra',
+          'Ciências Biológicas',
+          'Engenharias',
+          'Ciências da Saúde',
+          'Ciências Agrárias',
+          'Ciências Sociais Aplicadas',
+          'Ciências Humanas',
+          'Linguística, Letras e Artes',
+          'Outros',
+        ],
+        rows,
+      },
+    };
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,62 +1,16 @@
 <template>
   <v-container>
-    <ve-histogram
-      :data="chartData"
-      :settings="chartSettings"
-    />
+    <articles-by-area-and-institute />
   </v-container>
 </template>
 
 <script>
-import rows from './rows.json';
+import ArticlesByAreaAndInstitute from '@/components/charts/ArticlesByAreaAndInstitute.vue';
 
 export default {
-
   name: 'Home',
-  data() {
-    this.chartSettings = {
-      metrics: [
-        'Ciências Exatas e da Terra',
-        'Ciências Biológicas',
-        'Engenharias',
-        'Ciências da Saúde',
-        'Ciências Agrárias',
-        'Ciências Sociais Aplicadas',
-        'Ciências Humanas',
-        'Linguística, Letras e Artes',
-        'Outros',
-      ],
-      stack: {
-        areas: [
-          'Ciências Exatas e da Terra',
-          'Ciências Biológicas',
-          'Engenharias',
-          'Ciências da Saúde',
-          'Ciências Agrárias',
-          'Ciências Sociais Aplicadas',
-          'Ciências Humanas',
-          'Linguística, Letras e Artes',
-          'Outros',
-        ],
-      },
-    };
-    return {
-      chartData: {
-        columns: [
-          'institute',
-          'Ciências Exatas e da Terra',
-          'Ciências Biológicas',
-          'Engenharias',
-          'Ciências da Saúde',
-          'Ciências Agrárias',
-          'Ciências Sociais Aplicadas',
-          'Ciências Humanas',
-          'Linguística, Letras e Artes',
-          'Outros',
-        ],
-        rows,
-      },
-    };
+  components: {
+    ArticlesByAreaAndInstitute,
   },
 };
 </script>
