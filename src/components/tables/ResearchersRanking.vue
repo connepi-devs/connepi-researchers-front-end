11733e210f85dbf925216bd73377e71ec7448358
Otacílio Neto <otaneto@rogalabs.com>
2019-09-21 15:26:59 -0300

    changed ResearchersRanking style

--- src/components/tables/ResearchersRanking.vue
+++ src/components/tables/ResearchersRanking.vue
@@ -1,27 +1,58 @@
 <template>
-  <v-layout column>
-    <h2>Ranking Pesquisadores</h2>
-    <v-data-table
-      :items="data"
-      item-text="nome"
-      :headers="headers"
-      :items-per-page="5"
-    />
-  </v-layout>
+  <v-card elevation="0" max-height="370">
+    <v-card-text>
+      <v-container>
+        <v-layout column>
+          <v-layout class="my-2" align-end>
+            <v-icon class="mr-2">mdi-podium</v-icon>
+            <h2>Ranking Pesquisadores</h2>
+          </v-layout>
+          <v-data-table
+            :items="data"
+            item-text="nome"
+            :headers="headers"
+            :items-per-page="5"
+            hide-default-footer
+          >
+            <template v-slot:item.posicao="{ item }">
+              <v-chip
+                v-if="item.posicao <= 3"
+                dark
+                :color="getColor(item.posicao)"
+              >
+                {{ item.posicao }}
+              </v-chip>
+              <span v-else>{{ item.posicao }}</span>
+            </template>
+            <template v-slot:item.nome="{ item }">
+              <strong
+                v-if="item.posicao <= 3"
+                :class="`${getColor(item.posicao)}--text`"
+              >
+                {{ item.nome }}
+              </strong>
+              <span v-else>{{ item.nome }}</span>
+            </template>
+          </v-data-table>
+        </v-layout>
+      </v-container>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
+import { orderBy } from 'lodash';
 import data from '@/data/researchers-ranking.json';
 
 export default {
   name: 'ResearchersRanking',
   data() {
     return {
-      data,
+      data: orderBy(data, ['posicao']),
       headers: [
         {
           text: '',
-          align: 'left',
+          align: 'center',
           sortable: false,
           value: 'posicao',
         },
@@ -33,12 +64,22 @@ export default {
         },
         {
           text: 'Projetos',
-          align: 'left',
+          align: 'center',
           sortable: false,
           value: 'projetos',
         },
       ],
     };
+  },
+  methods: {
+    getColor(posicao) {
+      const posicoes = {
+        1: 'amber',
+        2: 'grey',
+        3: 'deep-orange',
+      };
+      return posicoes[posicao];
+    },
   },
 };
 </script>
