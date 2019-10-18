b59c7a0e8c36c55db002c8b8025c21b72db25468
Otacílio Neto <otaneto@rogalabs.com>
2019-10-18 13:57:43 -0300

    getting institutes from api

--- src/components/charts/ArticlesByAreaByInstituteByYear.vue
+++ src/components/charts/ArticlesByAreaByInstituteByYear.vue
@@ -3,12 +3,21 @@
     <v-card-text>
       <v-container>
         <v-layout column>
-          <v-layout class="mt-2 mb-4" align-end>
+          <v-layout class="mt-2 mb-4">
             <h2>Total de artigos por área por IF por ano</h2>
+            <v-spacer />
+            <v-autocomplete
+              v-model="filter"
+              label="Filtrar por Instituição"
+              outlined
+              :items="institutes"
+              item-text="sigla"
+              item-value="id"
+              return-object
+              @input="getData"
+            />
           </v-layout>
-          <ve-histogram
-            :data="chartData"
-          />
+          <ve-histogram :data="chartData" />
         </v-layout>
       </v-container>
     </v-card-text>
@@ -16,8 +25,9 @@
 </template>
 
 <script>
-import { sortBy } from 'lodash';
+import { orderBy } from 'lodash';
 import dashboardService from '@/services/dashboard-service';
+import instituteService from '@/services/institute-service';
 
 export default {
   name: 'ArticlesByAreaByInstituteByYear',
@@ -38,20 +48,44 @@ export default {
         ],
         rows: [],
       },
+      filter: { id: -1, sigla: 'Todos' },
+      institutes: [{ id: -1, sigla: 'Todos' }],
     };
   },
-  mounted() {
+  created() {
+    this.getInstitutes();
     this.getData();
   },
   methods: {
     getData() {
-      dashboardService.getArticlesPerInstitutePerAreaPerYear()
+      const queryParams = this.defineParams();
+      
+      dashboardService.getArticlesPerInstitutePerAreaPerYear(queryParams)
         .then(({ data }) => {
-          this.chartData.rows = sortBy(data, ['ano']);
+          this.chartData.rows = orderBy(data, ['ano']);
         })
         .catch((err) => {
           console.error(err);
         });
+    },
+    getInstitutes() {
+      instituteService.get()
+        .then(({ data }) => {
+          const orderedInstitutes = orderBy(data, ['nome']);
+          this.institutes = [...this.institutes, ...orderedInstitutes, { id: -2, sigla: 'Outros' }];
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+    defineParams() {
+      if (this.filter.sigla === 'Outros') {
+        return 'outros=true'
+      } else if (this.filter.sigla === 'Todos') {
+        return '';
+      } else {
+        return `instituicao_id=${this.filter.id}`;
+      }
     },
   },
 };
