f8b980d23d770a2c20ddaacad71504b7918f01e1
Otacílio Neto <otaneto@rogalabs.com>
2019-11-20 09:41:23 -0300

    added raleway-npm-webfont package

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
 import './assets/styles/main.css';
+import 'raleway-npm-webfont/style.scss';
 
 Vue.use(VCharts);
 
