fb8a7569c396ec8f8adf0b175b54b70f742e56a6
Otacílio Neto <otaneto@rogalabs.com>
2019-11-20 09:05:26 -0300

    fixed baseURL

--- src/services/axios-instance.js
+++ src/services/axios-instance.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const baseURL = process.env.VUE_APP_API_URL;
 
 export const axiosInstance = axios.create({
-  baseURL: `${baseURL}/api`,
+  baseURL: `${baseURL}/api/v1/`,
   timeout: 30000,
 });
