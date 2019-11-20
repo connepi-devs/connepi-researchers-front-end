63ea41f8a6d92dabca7d035420886888eb5cef05
Otacílio Neto <otaneto@rogalabs.com>
2019-11-20 09:14:50 -0300

    added Dashboard route

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Dashboard from './views/Dashboard.vue';
 
 Vue.use(Router);
 
@@ -10,8 +11,14 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'Home',
       component: Home,
+      redirect: { name: 'Dashboard' },
+    },
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: Dashboard,
     },
   ],
 });
