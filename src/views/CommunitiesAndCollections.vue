c48cfbe300c7df2be01bfe196552ea0909c4fb5a
otaneto <otaciliolucas@gmail.com>
2019-11-24 16:34:33 -0300

    created CommunitiesAndCollections view

---
+++ src/views/CommunitiesAndCollections.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container grid-list-lg>
+    <v-card width="100%" height="100%">
+      <v-card-title>Comunidades e Coleções</v-card-title>
+      <v-card-text>Teste</v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'CommunitiesAndCollections',
+}
+</script>
