976cd5f0f61a4f48d16ff1f1f4b0ef002c65286c
otaneto <otaciliolucas@gmail.com>
2019-11-24 15:17:56 -0300

    receiving data via props

--- src/components/publications/PublicationsTable.vue
+++ src/components/publications/PublicationsTable.vue
@@ -1,11 +1,11 @@
 <template>
   <v-data-table
     :headers="headers"
-    :items="desserts"
+    :items="publications"
     :items-per-page="5"
-    class="elevation-1"
+    calculate-widths
   >
-    <template v-slot:item.view="{ item }">
+    <template v-slot:item.file_url="{ item }">
       <v-btn text small>
         <v-icon>mdi-eye</v-icon>
       </v-btn>
@@ -17,7 +17,7 @@
 export default {
   name: 'PublicationsTable',
   props: {
-    results: {
+    publications: {
       default: () => [],
       type: Array,
     },
@@ -29,75 +29,13 @@ export default {
           text: 'Título da Publicação',
           align: 'left',
           sortable: false,
-          value: 'name',
+          value: 'titulo',
         },
-        { text: 'Autor(a)', value: 'calories' },
-        { text: 'Área', value: 'fat' },
-        { text: 'Ano', value: 'carbs' },
-        { text: 'Insitituição', value: 'carbs' },
-        { text: 'Visualizar', value: 'view' },
-      ],
-      desserts: [
-        {
-          name: 'Frozen Yogurt',
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-        },
-        {
-          name: 'Ice cream sandwich',
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-        },
-        {
-          name: 'Eclair',
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-        },
-        {
-          name: 'Cupcake',
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-        },
-        {
-          name: 'Gingerbread',
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-        },
-        {
-          name: 'Jelly bean',
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-        },
-        {
-          name: 'Lollipop',
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-        },
-        {
-          name: 'Honeycomb',
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-        },
-        {
-          name: 'Donut',
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-        },
-        {
-          name: 'KitKat',
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-        },
+        { text: 'Autor(a)', value: 'autor' },
+        { text: 'Área', value: 'area' },
+        { text: 'Ano', value: 'ano' },
+        { text: 'Insitituição', value: 'sigla' },
+        { text: 'Visualizar', value: 'file_url' },
       ],
     };
   },
