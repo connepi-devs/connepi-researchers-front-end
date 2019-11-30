cbd1b246376f64b5e992f363e7c1fbc0f8719afd
otaneto <otaciliolucas@gmail.com>
2019-11-30 09:53:24 -0300

    fixed layout and changed title font-family

--- src/views/CommunitiesAndCollections.vue
+++ src/views/CommunitiesAndCollections.vue
@@ -1,47 +1,49 @@
 <template>
   <v-container fill-height grid-list-lg>
     <span class="headline font-weight-black section-title">Comunidades e coleções</span>
-    <v-card min-height="70%" width="100%">
-      <v-card-title>Coleção: CONNEPI</v-card-title>
-      <v-card-text>
-        <v-expansion-panels focusable>
-          <v-expansion-panel
-            v-for="(item,i) in 4"
-            :key="i"
-          >
-            <v-expansion-panel-header>
-              <div>
-                <v-icon class="mr-3">mdi-folder-multiple</v-icon>
-                <span class="font-weight-bold">Connepi 200{{ i + 6 }}</span>
-              </div>
-            </v-expansion-panel-header>
-            <v-expansion-panel-content>
-              <v-list two-line subheader>
-                <v-list-item
-                  v-for="i in 5"
-                  :key="i"
-                  @click="showCollection"
-                >
-                  <v-list-item-avatar color="amber">
-                    <v-icon color="white">mdi-folder</v-icon>
-                  </v-list-item-avatar>
+    <v-row class="fill-height" align="start">
+      <v-card width="100%">
+        <v-card-title class="title font-weight-bold">Coleção: CONNEPI</v-card-title>
+        <v-card-text>
+          <v-expansion-panels focusable>
+            <v-expansion-panel
+              v-for="(item,i) in 4"
+              :key="i"
+            >
+              <v-expansion-panel-header>
+                <div>
+                  <v-icon class="mr-3">mdi-folder-multiple</v-icon>
+                  <span class="font-weight-bold">Connepi 200{{ i + 6 }}</span>
+                </div>
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <v-list two-line subheader>
+                  <v-list-item
+                    v-for="i in 5"
+                    :key="i"
+                    @click="showCollection"
+                  >
+                    <v-list-item-avatar color="amber">
+                      <v-icon color="white">mdi-folder</v-icon>
+                    </v-list-item-avatar>
 
-                  <v-list-item-content>
-                    <v-list-item-title>Ciências Exatas e da Terra (50)</v-list-item-title>
-                  </v-list-item-content>
+                    <v-list-item-content>
+                      <v-list-item-title>Ciências Exatas e da Terra (50)</v-list-item-title>
+                    </v-list-item-content>
 
-                  <v-list-item-action>
-                    <v-btn @click="showCollection" icon>
-                      <v-icon color="grey darken-2">mdi-eye</v-icon>
-                    </v-btn>
-                  </v-list-item-action>
-                </v-list-item>
-              </v-list>
-            </v-expansion-panel-content>
-          </v-expansion-panel>
-        </v-expansion-panels>
-      </v-card-text>
-    </v-card>
+                    <v-list-item-action>
+                      <v-btn @click="showCollection" icon>
+                        <v-icon color="grey darken-2">mdi-eye</v-icon>
+                      </v-btn>
+                    </v-list-item-action>
+                  </v-list-item>
+                </v-list>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+        </v-card-text>
+      </v-card>
+    </v-row>
   </v-container>
 </template>
 
