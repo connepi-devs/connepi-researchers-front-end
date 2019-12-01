ba4524f4f02d3656ac7cffb1ae9bac5a3a94d454
otaneto <otaciliolucas@gmail.com>
2019-12-01 02:47:55 -0300

    changes for small devices support

--- src/components/cards/Card.vue
+++ src/components/cards/Card.vue
@@ -3,11 +3,17 @@
     <v-card-text>
       <v-list :color="data.cor" two-line>
         <v-list-item>
-          <v-list-item-avatar>
+          <v-list-item-avatar v-if="$vuetify.breakpoint.smAndUp">
             <v-icon size="40">{{ data.icone }}</v-icon>
           </v-list-item-avatar>
           <v-list-item-content>
-            <v-list-item-title class="headline font-weight-bold">
+            <v-list-item-title
+              class="font-weight-bold"
+              :class="{
+                'subtitle-1': $vuetify.breakpoint.smAndUp,
+                'subtitle-2': $vuetify.breakpoint.xs,
+              }"
+            >
               {{ data.titulo }}
             </v-list-item-title>
             <v-list-item-subtitle class="title">{{ data.total }}</v-list-item-subtitle>
