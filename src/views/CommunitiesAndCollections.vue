5fbb021c667c3e34e66dd02c5b209faf9c43148a
otaneto <otaciliolucas@gmail.com>
2019-12-03 08:24:19 -0300

    added v-col

--- src/views/CommunitiesAndCollections.vue
+++ src/views/CommunitiesAndCollections.vue
@@ -2,54 +2,57 @@
   <v-container grid-list-lg>
     <div class="headline font-weight-black section-title">Comunidades e coleções</div>
     <v-row class="fill-height" align="start">
-      <v-card width="100%">
-        <v-card-title
-          class="title font-weight-bold primary white--text"
-        >
-          Coleção: CONNEPI
-        </v-card-title>
-        <v-card-text class="mt-5">
-          <v-expansion-panels focusable>
-            <v-expansion-panel
-              v-for="(item, index) in areas"
-              :key="index"
-            >
-              <v-expansion-panel-header>
-                <div>
-                  <v-icon class="mr-3">mdi-folder-multiple</v-icon>
-                  <span class="font-weight-bold">Connepi {{ item.ano }}</span>
-                </div>
-              </v-expansion-panel-header>
-              <v-expansion-panel-content>
-                <v-list two-line subheader>
-                  <v-list-item
-                    v-for="(total, area) in omit(item, ['ano'])"
-                    :key="area"
-                    @click="showCollection"
-                  >
-                    <v-list-item-avatar
-                      v-if="$vuetify.breakpoint.smAndUp"
-                      color="amber"
+      <v-col cols="12">
+
+        <v-card width="100%">
+          <v-card-title
+            class="title font-weight-bold primary white--text"
+          >
+            Coleção: CONNEPI
+          </v-card-title>
+          <v-card-text class="mt-5">
+            <v-expansion-panels focusable>
+              <v-expansion-panel
+                v-for="(item, index) in areas"
+                :key="index"
+              >
+                <v-expansion-panel-header>
+                  <div>
+                    <v-icon class="mr-3">mdi-folder-multiple</v-icon>
+                    <span class="font-weight-bold">Connepi {{ item.ano }}</span>
+                  </div>
+                </v-expansion-panel-header>
+                <v-expansion-panel-content>
+                  <v-list two-line subheader>
+                    <v-list-item
+                      v-for="(total, area) in omit(item, ['ano'])"
+                      :key="area"
+                      @click="showCollection"
                     >
-                      <v-icon color="white">mdi-folder</v-icon>
-                    </v-list-item-avatar>
+                      <v-list-item-avatar
+                        v-if="$vuetify.breakpoint.smAndUp"
+                        color="amber"
+                      >
+                        <v-icon color="white">mdi-folder</v-icon>
+                      </v-list-item-avatar>
 
-                    <v-list-item-content>
-                      <v-list-item-title>{{ area }} ({{ total }})</v-list-item-title>
-                    </v-list-item-content>
+                      <v-list-item-content>
+                        <v-list-item-title>{{ area }} ({{ total }})</v-list-item-title>
+                      </v-list-item-content>
 
-                    <v-list-item-action>
-                      <v-btn @click="showCollection" icon>
-                        <v-icon color="grey darken-2">mdi-download</v-icon>
-                      </v-btn>
-                    </v-list-item-action>
-                  </v-list-item>
-                </v-list>
-              </v-expansion-panel-content>
-            </v-expansion-panel>
-          </v-expansion-panels>
-        </v-card-text>
-      </v-card>
+                      <v-list-item-action>
+                        <v-btn @click="showCollection" icon>
+                          <v-icon color="grey darken-2">mdi-download</v-icon>
+                        </v-btn>
+                      </v-list-item-action>
+                    </v-list-item>
+                  </v-list>
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+            </v-expansion-panels>
+          </v-card-text>
+        </v-card>
+      </v-col>
     </v-row>
   </v-container>
 </template>
