e5871dce182d43a588f6649fc5f71466506f7859
otaneto <otaciliolucas@gmail.com>
2019-12-17 13:49:40 -0300

    finish publication

--- src/views/Publication.vue
+++ src/views/Publication.vue
@@ -9,24 +9,43 @@
           <v-card-text>
             <div class="flex column subtitle-1 pt-3">
               <span>
-                <strong>Título: </strong> VCDSL: Uma proposta de Framework para a Geração de Linguagens Visuais Específicas de Domínio On-line
+                <strong>Título: </strong> {{ file.titulo || 'Não possui' }}
               </span>
               <span>
-                <strong>Autor: </strong> José de Anchieta Queiroz Junior, Francisco José A. C. Souza, Cidcley Teixeira de Souza
+                <strong>Autor: </strong> {{ file.autor || 'Não possui' }}
               </span>
               <span>
-                <strong>Ano: </strong> 2013
+                <strong>Ano: </strong> {{ file.ano || 'Não possui' }}
               </span>
               <span>
-                <strong>Instituição: </strong> IFCE
+                <strong>Instituição: </strong> {{ file.instituicao || 'Não possui' }}
               </span>
 
               <!-- PDF -->
+              <div
+                v-if="loading && fileUrl"
+                class="flex column justify-center align-center"
+                style="width: 100%; height: 100%; position: absolute"
+              >
+                <v-progress-circular size="48" color="primary" indeterminate />
+                <span class="headline">Carregando PDF...</span>
+              </div>
+              <div
+                v-if="!fileUrl"
+                class="flex column justify-center align-center"
+                style="width: 100%; height: 100%; position: absolute"
+              >
+                <v-icon size="48">mdi-link-off</v-icon>
+                <span class="headline">Infelizmente a publicação não possui PDF carregado</span>
+              </div>
               <iframe
+                v-if="fileUrl"
+                id="iframe"
                 class="mt-5"
-                src="https://drive.google.com/file/d/1cFjfy0K0ce3f5GaxYPMEegpEqY7P0J4n/preview"
+                :src="fileUrl"
                 width="100%"
                 height="480"
+                @load="loading = false"
               />
             </div>
           </v-card-text>
@@ -37,7 +56,42 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+import publicationsService from '@/services/publications-service';
+import handleErrors from '@/utils/handle-errors';
+
 export default {
   name: 'Publication',
+  created() {
+    this.getPublication();
+  },
+  props: {
+    id: {},
+  },
+  data() {
+    return {
+      loading: true,
+      file: {},
+      fileUrl: null,
+      firstRender: true,
+    };
+  },
+  methods: {
+    ...mapActions('base', ['setSnackbar']),
+    getPublication() {
+      publicationsService.show(this.id)
+        .then(({ data }) => {
+          this.file = data;
+          this.fileUrl = data.file_url;
+        })
+        .catch((err) => {
+          this.setSnackbar({
+            color: 'error',
+            message: handleErrors(err),
+            show: true,
+          });
+        });
+    },
+  },
 };
 </script>
